89fbb38844ddf510d3cc6c4cafe8276393a2591f
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-10-12 17:35:38 +0200

    modif

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var input = document.getElementById("input")
     // On récupère l'élément sur lequel on veut détecter le clic
  function Submit() {          // On écoute l'événement click
 alert(input.value)               // On change le contenu de notre élément pour afficher "C'est cliqué !"
-}
+ }
                 // On change le contenu de notre élément pour afficher "C'est cliqué !"
+
 
